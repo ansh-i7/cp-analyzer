d53bf688e18be34f0fd5449cf0d07a8e5c882dfe
Shiwansh Das <learneransh24@gmail.com>
2025-12-30 00:59:50 +0530

    Added Basic Recursion detection

--- input/solution.cpp
+++ input/solution.cpp
@@ -1,11 +1,16 @@
-/*
-{
+
+/* {
     for(int i=0;i<n;i++)
 }
-*/
-for(int i=0;i<n;i++) {
+
+ for(int i=0;i<n;i++) {
     // inner loop starts below
     for(int j=0;j<n;j++) {
         cout << i << j;
     }
+} */
+
+int solve(int n) {
+    if (n == 0) return 0;
+    return solve(n - 1);
 }
--- main.cpp
+++ main.cpp
@@ -16,7 +16,11 @@ int main() {
    int forCount=0;
    int openBraces=0;        //For brace tracking
    bool nestedLoop = false;
-   bool inComment=false;
+
+   bool inComment=false;  //For checking if inside a comment or not
+
+   bool rec_detect=false;
+   string recFunc_name = "";
 
    string timeComplexity = "O(1)";
 
@@ -41,7 +45,42 @@ int main() {
         line=line.substr(0,commentPos);
     }
 
+    if(line.empty()) continue;      //skips analyzing empty line
+
     cout<<line<<endl;
+
+    if(recFunc_name.empty()){
+        size_t pos_OpenBrac = line.find("(");
+
+        if(pos_OpenBrac!=string::npos && line.find("for")==string::npos
+            && line.find("if")==string::npos && line.find("while")==string::npos){
+                string beforePar=line.substr(0,pos_OpenBrac);
+
+                string Temp_recFunc="";
+                string temp="";
+
+                for (char c : beforePar) {
+                    if (c==' '){
+                        if(!temp.empty()) {
+                            Temp_recFunc = temp;
+                            temp.clear();
+                            }
+                        } 
+                        else {
+                            temp+=c;
+                        }
+                    }
+                if(!temp.empty()) Temp_recFunc=temp;
+
+            recFunc_name = Temp_recFunc;
+        }
+    }
+
+    if( !recFunc_name.empty() && line.find(recFunc_name+"(")!=string::npos){
+        if(line.find("{")==string::npos){     //with this i'll avoid counting the definition line as "{" comes after defining a function. 
+            rec_detect=true;            
+        }     
+    }
 
     if(line.find("for")!= string::npos){    //detects key word "for" in every line.
        forCount++;
@@ -60,6 +99,13 @@ int main() {
         openBraces--;
     }
  }
+
+    if(rec_detect){
+        cout<<"Recursion detected"<<endl;
+    }
+    else {
+        cout<<"No Recursion detected"<<endl;
+    }
 
    cout<<"Total for loops detected : "<<forCount<<endl;
 
